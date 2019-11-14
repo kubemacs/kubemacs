5443621fffd3807a9ef15d08a804722ce3825b3f
Hippie Hacker <hh@ii.coop>
2019-11-15 09:25:23 +1300

    Adding bin/start_session

---
+++ bin/start_session.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x
+set -e
+export ALTERNATE_EDITOR=""
+export EMACSSOCKETNAME=$(basename $1)
+export II_SESSION=${USER}.${EMACSSOCKETNAME}
+OSC52E=$(dirname "${BASH_SOURCE[0]}")/osc52.sh
+xtermcontrol --title $USER.$EMACSSOCKETNAME@sharing.io
+echo $OSC52E
+# ensure this TMATE starts shell with BASE=$BASE
+tmate -S /tmp/${II_SESSION}.iisocket new-session \
+      -A -s $USER -n emacs \
+      "tmate wait tmate-ready \
+&& TMATE_CONNECT=\
+\$(tmate display -p '#{tmate_ssh} # ${USER}.${BASE} # $(date) # #{tmate_web}') \
+; echo \$TMATE_CONNECT \
+; (echo \$TMATE_CONNECT | $OSC52E ) \
+; echo Share the above with your friends and hit enter here when done? \
+; read ; \
+emacsclient -s $EMACSSOCKETNAME --tty $1 2>&1"
