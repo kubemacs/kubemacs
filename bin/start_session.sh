4dd0f36ec6265ad0bd8bcf1376c423797332b3d6
Hippie Hacker <hh@ii.coop>
2019-11-15 09:38:23 +1300

    Needed to update start_session.sh to include basename of target

--- bin/start_session.sh
+++ bin/start_session.sh
@@ -12,7 +12,7 @@ tmate -S /tmp/${II_SESSION}.iisocket new-session \
       -A -s $USER -n emacs \
       "tmate wait tmate-ready \
 && TMATE_CONNECT=\
-\$(tmate display -p '#{tmate_ssh} # ${USER}.${BASE} # $(date) # #{tmate_web}') \
+\$(tmate display -p '#{tmate_ssh} # ${II_SESSION} # $(date) # #{tmate_web}') \
 ; echo \$TMATE_CONNECT \
 ; (echo \$TMATE_CONNECT | $OSC52E ) \
 ; echo Share the above with your friends and hit enter here when done? \
