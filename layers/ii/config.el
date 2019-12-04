18379099d12b3db80a067658a1850282c8a4cb5a
Hippie Hacker <hh@ii.coop>
2019-12-05 11:11:43 +1300

    Set our python-shell-interpeter to python3

--- layers/ii/config.el
+++ layers/ii/config.el
@@ -7,4 +7,5 @@
  auto-completion-private-snippets-directory (expand-file-name (concat iimacs-dir "snippets"))
  auto-completion-enable-snippets-in-popup t
  osc52e-package-dir (concat (configuration-layer/get-layer-local-dir 'ii) "osc52e")
+ python-shell-interpreter "python3"
  )
