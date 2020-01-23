a8def65782c2fa14bf86938bcc388e8e3f09eee3
Hippie Hacker <hh@ii.coop>
2020-01-23 15:22:27 +1300

    Add a .kube/config and set token in container init

--- bin/simple-init.sh
+++ bin/simple-init.sh
@@ -5,6 +5,8 @@ if [ ! -f ".ssh/id_rsa" ]
 then
     ssh-keygen -b 4096 -t rsa -f ~/.ssh/id_rsa -q -N ""
 fi
+# parts of the e2e suite seem to want an actual working kubeconfig....
+kubectl config set-credentials default --token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 export ALTERNATE_EDITOR=""
 export TMATE_SOCKET="/tmp/iisocket"
 export TMATE_SOCKET_NAME=`basename ${TMATE_SOCKET}`
